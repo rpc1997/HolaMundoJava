e15160a172838ba2af45f9005c3aaf898737bba2
ruben <>
2021-11-22 19:38:58 +0100

    Holaaa

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,22 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Rubén Pérez <rpcausias@gmail.com>
+ */
+public class HolaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        s
+        // TODO code application logic here
+    }
+    
+}
